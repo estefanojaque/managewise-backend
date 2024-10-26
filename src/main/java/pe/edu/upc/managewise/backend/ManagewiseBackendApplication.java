76b516d421698ba353128810403151630a649df4
John_Arvlo <u202117377@upc.edu.pe>
2024-10-26 13:11:00 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/managewise/backend/ManagewiseBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.managewise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagewiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagewiseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/managewise/backend/ManagewiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.managewise.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagewiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
