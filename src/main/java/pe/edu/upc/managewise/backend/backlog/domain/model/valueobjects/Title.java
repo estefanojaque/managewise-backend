bd47771614853cc17cc98a5216ba4dfd72efaede
John_Arvlo <u202117377@upc.edu.pe>
2024-10-27 12:00:12 -0500

    feat: added backlog models

--- src/main/java/pe/edu/upc/managewise/backend/backlog/domain/model/valueobjects/Title.java
+++ src/main/java/pe/edu/upc/managewise/backend/backlog/domain/model/valueobjects/Title.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.managewise.backend.backlog.domain.model.valueobjects;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public record Title(String title) {
+    public Title(){
+        this(null);
+    }
+    public Title{
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Title cannot be null or blank");
+        }
+    }
 }
